5ed6b9df6f375806ac090e7310c21e42dd47a27e
Ter[r]y <johnterence.morales@benilde.edu.ph>
2024-09-25 14:46:42 +0800

    added blob images. made the main front page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [startQuiz, setStartQuiz] = useState(false)
+
+  function beginQuiz() {
+    setStartQuiz(prevStartQuiz => !prevStartQuiz)
+  }
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <div className="starting-page">
+        
+        <h1>Quizzical</h1>
+        <h3>Some description if needed</h3>
+        <button className="btn-primary" onClick={beginQuiz}>Start Quiz</button>
+      </div>
     </>
   )
 }
