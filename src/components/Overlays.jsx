044bd6c722c6c10a865fe0415d85c9ba1cb43875
Ter[r]y <johnterence.morales@benilde.edu.ph>
2024-10-01 15:59:36 +0800

    placed modal in overlays as a const instead of another freakin composable ffs

--- src/components/Overlays.jsx
+++ src/components/Overlays.jsx
@@ -1,19 +1,31 @@
 import { createPortal } from 'react-dom'
-import Modal from './Modal'
 import { useAtom } from 'jotai'
 import { uiAtom } from '../State'
 
 const mountElement = document.getElementById('overlays')
 
  const Overlays = ({ newQuiz, closeModal }) => {
+
+  const modal = 
+  <div className="modal">
+    <h1>Are you sure you want a new quiz?</h1>
+    <div className="btn-container">
+      <button className="btn-primary" onClick={newQuiz}>
+        Yes
+      </button>
+      <button className="btn-secondary" onClick={closeModal}>
+        No
+      </button>
+
+    </div>
+  </div>
+
   const [ui] = useAtom(uiAtom)
+  
   return createPortal(
     ui.modal && 
     <>
-      <Modal
-        newQuiz={newQuiz}
-        closeModal={closeModal}
-      />
+      {modal}
     </>, 
     mountElement
   )
